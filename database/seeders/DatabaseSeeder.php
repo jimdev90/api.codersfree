1ea7e1b3b111efe2fb0431be37a879c7d1ffd11c
jimdev90 <jimpv100190@gmail.com>
2023-11-04 12:54:42 -0500

    Storage::deleteDirectory('public/posts')

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,9 +23,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        Storage::deleteDirectory('posts');
-
-        Storage::makeDirectory('posts');
+        Storage::deleteDirectory('public/posts');
+        Storage::makeDirectory('public/posts');
 
         $this->call(UserSeeder::class);
         Category::factory(4)->create();
